d4089c49e2c386940535ca321723ed6b6e57e646
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-16 09:19:01 +1000

    Added inital textinput prompt

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ image = "blank_states_img.gif"
 screen.setup(width=726, height=496)
 screen.addshape(image)
 
-
 image_ob = turtle.Turtle()
-
 image_ob.shape(image)
 
 
-# answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 
+while True:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    user_guess = answer_state.title()
+
+
+screen.exitonclick()
+
+# To get coordinates using mouse click
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 
 # turtle.onscreenclick(get_mouse_click_coor)
 
 # turtle.mainloop()
-
-screen.exitonclick()
