44c14fb83031d66731de6697b4e1287adb06cb0d
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-16 10:18:25 +1000

    Added logic for extracting coordinate and writing state to its respective coordinate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import turtle
 import pandas
 
+FONT = ('Arial', 10, 'normal')
+
 states_csv = "50_states.csv"
 
 screen = turtle.Screen()
@@ -13,10 +15,19 @@ screen.addshape(image)
 
 image_ob = turtle.Turtle()
 image_ob.shape(image)
+writing_ob = turtle.Turtle()
 
 data = pandas.read_csv(states_csv)
 states = data.state.tolist()
+# print(data.loc[data.state == "Ohio", ['x', 'y']].values.flatten().tolist())
 # print(states)
+
+def write_state(user_guess, coordinates):
+    writing_ob.penup()
+    writing_ob.hideturtle()
+    writing_ob.color('black')
+    writing_ob.goto(x=coordinates[0], y=coordinates[1])
+    writing_ob.write(user_guess, align='center', font=FONT)
 
 score = 0
 score_tally = f"{score}/50 States Correct"
@@ -24,10 +35,14 @@ score_tally = f"{score}/50 States Correct"
 while True:
     answer_state = screen.textinput(title=score_tally, prompt="What's another state's name?")
     user_guess = answer_state.title()
-    print(user_guess)
+    # print(type(user_guess))
+    coordinates = data.loc[data.state == user_guess, ['x', 'y']].values.flatten().tolist()
 
     if user_guess in states:
-        print("correct")
+        score += 1
+        write_state(user_guess, coordinates)
+        # print("correct")
+        
 
 
 screen.exitonclick()
